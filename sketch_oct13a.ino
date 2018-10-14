4d8d487c1ca494a2d0c6cfbcc9322c1469f37999
Thirinaing614 <thiriyulissa16@gmail.com>
2018-10-14 10:34:15 +0630

    arduino servo

---
+++ sketch_oct13a.ino
@@ -0,0 +1,41 @@
+#include <Servo.h>           //include servo library
+
+Servo servo1;                // creating servo object
+int joystick_x = A0;              // joystick x direction pin                                          
+                                                      
+int pos_x;                   // declaring variable to stroe horizontal value
+
+int servo1_pos=90;          // storing servo position
+
+void setup ( ) 
+{
+Serial.begin (9600) ;
+servo1.attach (10) ;          // servo 1 attached pin
+  
+pinMode (joystick_x, INPUT) ;                     
+                 
+}
+
+void loop ( ) 
+{
+pos_x = analogRead (joystick_x) ;  
+
+if (pos_x <300)  {          //if the horizontal value from joystick is less than 300
+
+  servo1.write(140);      //first servo moves right
+}  
+
+ 
+else if (pos_x > 700){
+
+  servo1.write(40);
+}   
+else
+  {
+  
+  servo1.write ( servo1_pos ) ;
+  delay (100) ;
+  }
+  Serial.println(servo1_pos);
+  Serial.println(" ");
+  }
